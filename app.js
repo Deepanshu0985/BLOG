ade7cbaec2daf148302823dc9bcdd6b79d64c255
Deepanshu0985 <deepanshuy098@gmail.com>
2023-03-12 13:58:36 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Welcome to my personal blog! Here, I share my thoughts and experiences on various topics, including personal growth, mindfulness, travel, and more. Join me on this journey of self-discovery and exploration.";
+const aboutContent = "Welcome to my personal blog! Join me on a journey of self-discovery and exploration. I share my thoughts on personal growth, mindfulness, travel, and more.";
+const contactContent = "Welcome to my blog's contact page! If you have any questions, feedback, or just want to say hi, please feel free to reach out to me. You can send me an email at deepanshuy098@gmail.com, or connect with me on Instagram _yours__deepanshu_.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://deepanshuy098:%40Deepu0895..@cluster0.yikt4gp.mongodb.net/BLOG", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
